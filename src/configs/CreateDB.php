e59143de01cb5d0ddc9e8c98688f01b627a1d6af
maduram <madularama@gmail.com>
2017-04-27 23:49:41 -0700

    update

--- src/configs/CreateDB.php
+++ src/configs/CreateDB.php
@@ -2,7 +2,7 @@
 
 namespace excalibur\hw4\configs;
 
-use excalibur\Hw4\configs as CFG;
+use excalibur\hw4\configs as CFG;
 
 class CreateDB
 {
@@ -17,35 +17,30 @@ class CreateDB
         $db = $cfg::db;
 //Create connection
         $conn = new \mysqli($host, $usr, $pwd, $db);
-        
 
-       $dbcreate = 'CREATE DATABASE IF NOT EXISTS '.$cfg::db;  
+
+        $dbcreate = 'CREATE DATABASE IF NOT EXISTS ' . $cfg::db;
+
         if ($conn->query($dbcreate) === true) {
             echo $db . " created\n";
-            //$conn->select_db($db);
         } else {
             echo "Database could not be created: " . $conn->error . "\n";
             die;
         }
-        
+
 //Check connection
         if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error. "\n");
+            die("Connection failed: " . $conn->connect_error . "\n");
         }
-        echo "Connected successfully". "\n";
+        echo "Connected successfully" . "\n";
 
 
-// sql to create table
-//        $sql = "CREATE TABLE Sheet (
-//    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-//    sheet_name VARCHAR, 
-//    sheet_data INT(8) UNSIGNED ); ";
         $sql = "CREATE TABLE Sheet (
 id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
 sheet_name VARCHAR(30) NOT NULL,
 sheet_data TEXT
 )";
-        
+
         $sqlCodes = "CREATE TABLE Sheet_Codes (
 id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
 hash_code INT(8) NOT NULL,
@@ -56,15 +51,16 @@ code_type CHAR(1)
         if ($conn->query($sql) === TRUE) {
             echo "Table Sheet created successfully\n";
         } else {
-            echo "Error creating table: " . $conn->error. "\n";
+            echo "Error creating table: " . $conn->error . "\n";
         }
-        
+
         if ($conn->query($sqlCodes) === TRUE) {
             echo "Table Sheet_Codes created successfully\n";
         } else {
-            echo "Error creating table: " . $conn->error. "\n";
+            echo "Error creating table: " . $conn->error . "\n";
         }
 
         $conn->close();
     }
+
 }
