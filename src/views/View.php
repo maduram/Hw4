29544f37acf0697d5bc241189d8ac09baf364b04
maduram <madularama@gmail.com>
2017-04-28 22:33:40 -0700

    update

--- src/views/View.php
+++ src/views/View.php
@@ -10,29 +10,40 @@ use excalibur\hw4\views as VWS;
 class View
 {
 
-    private $db;
+    private $mdl;
 
     function landing ()
     {
-        $db = new CFG\CreateDB();
+        $mdl = new MDL\Model();
         ?>
         <!DOCTYPE html>
-            <head>
-                <title>Web Sheets</title>
-            </head>
-            <body>
-                <h1><a href="index.php">Web Sheets</a></h1>
-                    
-                <form action="index.php?c=main&m=landing" method="POST" >
-                    <input type="text" placeholder="New sheet name or code" name="input" required>
- <input type="submit" value="Go" >
-                </form>
-            </body>
+        <head>
+            <title>Web Sheets</title>
+        </head>
+        <body>
+            <h1><a href="index.php">Web Sheets</a></h1>
+
+            <form action="index.php?c=main&m=landing" method="POST">
+                <input type="text" placeholder="New sheet name or code" name="input" required>
+                <input type="submit" value="Go" >
+            </form>
             
+            <br />
+            <h2>Database Results:</h2>
+            <br />
+            
+        </body>
         </html>
         <?php
+        
+        
+        
+        
+        
+        $query = "INSERT INTO Sheet VALUES (NULL,'{$_POST['input']}', NULL)";
+        
+        CFG\CreateDB::do_query($query);
     }
 
 }
-
 //<input type="text" placeholder="New sheet name or code" id="input" value="<?php $db->insert(filter_input(INPUT_POST, 'input', \FILTER_SANITIZE_SPECIAL_CHARS));" required>
